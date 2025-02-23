091bbfd3df3ed1761f832f639c564b17220d0be9
RiaJul Pro <riajulpro@outlook.com>
2025-02-24 00:19:36 +0600

    [Installed] ShadCn UI, Theme Changer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
+import { ThemeToggler } from "@/components/shared/ThemeToggler";
+
 export default function Home() {
   return (
     <div>
+      <ThemeToggler />
       <h1 className="text-center text-2xl font-bold">Gazi Fuad Md. Tahsin</h1>
     </div>
   );
---
+++ src/providers/index.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { ThemeProvider } from "./theme-provider";
+
+const Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default Provider;
